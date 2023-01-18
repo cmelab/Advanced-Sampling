b9c325f5045ac1fc1fd628151821685709c8a1fc
Gwen <gwen@admins-mbp.boisestate.edu>
2023-01-18 14:07:22 -0700

    started working on structure factor code

--- MCMC/utils.py
+++ MCMC/utils.py
@@ -2,6 +2,19 @@ import numpy as np
 from numba import jit
 import freud
 
+def structure_factor(sim_obj, n_frames):
+    box = [sim_obj.L, sim_obj.L,0]
+    sf = freud.diffraction.StaticStructureFactorDebye(
+            num_k_values=100, k_max=10
+            )
+    for points in sim_obj.system_history[-n_frames:]:
+        points = np.append(
+            points, np.zeros((points.shape[0], 1)),axis=1
+        )
+        sf.compute((box, points))
+    return sf
+
+
 def rdf(sim_obj, n_frames, n_bins=50, r_max=None):
     if not r_max:
         r_max = (sim_obj.L/2)-sim_obj.r
