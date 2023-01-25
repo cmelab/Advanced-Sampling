5032c7ce4dbd4b275262abefc5ed366899e61330
marjanalbouye <marjan.albouye73@gmail.com>
2023-01-25 11:44:46 -0700

    Remove gpu config from templates.

--- MCMC/MCMC-flow/templates/fry.sh
+++ MCMC/MCMC-flow/templates/fry.sh
@@ -1,6 +1,5 @@
 {% extends "base_script.sh" %}
 {% block header %}
-{% set gpus = operations|map(attribute='directives.ngpu')|sum %}
 #!/bin/bash
 #SBATCH --job-name="{{ id }}"
 {% if partition %}
@@ -8,9 +7,6 @@
 {% endif %}
 {% if walltime %}
 #SBATCH -t {{ 48|format_timedelta }}
-{% endif %}
-{% if gpus %}
-#SBATCH --gres gpu:{{ gpus }}
 {% endif %}
 {% if job_output %}
 #SBATCH --output={{ job_output }}
