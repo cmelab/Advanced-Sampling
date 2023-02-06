2d7897928a37699281258ee2e186aa2d69469f4c
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2023-02-06 13:38:37 -0700

    add line for timestep in job doc

--- MCMC/MCMC-flow/init.py
+++ MCMC/MCMC-flow/init.py
@@ -57,6 +57,7 @@ def main():
         parent_job = project.open_job(parent_statepoint)
         parent_job.init()
         parent_job.doc.setdefault("done", False)
+        parent_job.doc.setdefault("timestep", [])
         parent_job.doc.setdefault("accepted_moves", [])
         parent_job.doc.setdefault("rejected_moves", [])
         parent_job.doc.setdefault("acceptance_ratio", [])
